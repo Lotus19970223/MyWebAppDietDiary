62efaeaf0202ccacaab36936f7893fbb7c66989d
Lotus <lotusrain96@gmail.com>
2022-10-30 22:43:52 +0900

    deployed with .bat

--- src/main/java/com/dietdiary/controller/SampleDiaryRepository.java
+++ src/main/java/com/dietdiary/controller/SampleDiaryRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.dietdiary.entity.SampleDiaryEntity;
 
-//
+//日記用のSpring Data リポジトリインターフェイス（サンプル）
+//https://spring.pleiades.io/spring-data/jpa/docs/current/reference/html/
 public interface SampleDiaryRepository extends CrudRepository<SampleDiaryEntity, Date> {
+//CrudRepositoryから継承した抽象メソッドに加え、下記の抽象メソッドを持たせる
+//リポジトリの
 }
--- src/main/java/com/dietdiary/service/SampleService.java
+++ src/main/java/com/dietdiary/service/SampleService.java
@@ -1,8 +1,17 @@
 package com.dietdiary.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dietdiary.controller.SampleDiaryRepository;
 
 @Service //サービスクラス
+@Transactional
 public class SampleService {
-
+	@Autowired
+	SampleDiaryRepository sdrRepository;
+//	public SampleDiaryEntity updateDiary(SampleDiaryEntity sampleDiaryEntity) {
+//        return sampleDiaryEntity.save(sampleDiaryEntity);
+//    }
 }
