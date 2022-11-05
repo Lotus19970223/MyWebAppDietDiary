86614919b59714070a65d2637a1ec331736cf8bf
Lotus <lotusrain96@gmail.com>
2022-11-06 01:41:46 +0900

    deployed with .bat

--- src/main/java/com/dietdiary/controller/CustomerController.java
+++ src/main/java/com/dietdiary/controller/CustomerController.java
@@ -115,8 +115,8 @@ public class CustomerController {
 	//post送信時に呼び出される
 	@PostMapping("/formSave")
 	//引数のSampleDiaryEntityは保存ボタン押下時に送信されたもの
-	public ModelAndView formSave(SampleDiaryEntity sdeFromForm) {
-		ModelAndView mav = new ModelAndView();
+	public void formSave(SampleDiaryEntity sdeFromForm) {
+		//ModelAndView mav = new ModelAndView();
 			System.out.println(sdeFromForm + "←が表示されていればsdeはnullではない");
 			//getDate()含めgetDiaryText()以外は引数のsdeではnull
 			//引数のsdeでは保存ボタン押下時に送信した値がgetDiaryText()で取得可
@@ -142,8 +142,8 @@ public class CustomerController {
 		    	}
 	        }
 		    //DB内容表示ページに遷移（保存ボタン押下後に再表示）
-			mav.setViewName("sampleDBRead");
-			return mav;
+			//mav.setViewName("sampleDBRead");
+			//return mav;
 		}
 	SampleDiaryEntity entityUpdateForFormSave(SampleDiaryEntity sde) {
 		//日記エンティティの持つ日付の値が2022-11-01の場合
