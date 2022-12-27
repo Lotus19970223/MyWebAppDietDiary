53d5f48b585873c7f83b853c8b6040d9d0674ed4
Lotus <lotusrain96@gmail.com>
2022-12-27 17:31:58 +0900

    deployed with .bat

--- src/main/java/com/dietdiary/controller/UserRepository.java
+++ src/main/java/com/dietdiary/controller/UserRepository.java
@@ -13,7 +13,9 @@ public interface UserRepository extends CrudRepository<UserEntity, Integer> {
 //CrudRepositoryから継承した抽象メソッドに加え、下記の抽象メソッドを持たせる
 
 	//ユーザーのレコードを取得する
-	@Query(value = "select * from USERS where user_id = ?1",
+	//\でダブルクオーテーションをエスケープする
+	//ダブルクオーテーションをつけないとuser_idカラムが見つからないため
+	@Query(value = "select * from USERS where \"user_id\" = ?1",
             nativeQuery = true)
 	//下記メソッドを呼び出す際にUserIDを指定して使用する
 	Iterable<UserEntity> findUserRecordByUserID(Integer userID);
